ac692f5041f4c46cf81012f1f6ef1f5f92a9a1af
Alex K <contact.alexsophia@gmail.com>
2020-05-22 15:40:22 +0300

    Fixed some JS files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,12 @@ function displayResult(result) {
     let showResult = document.getElementById('result');
     let bookLength = parseInt(document.getElementById('book_length').value);
 
-    if (bookLength >= 700 && bookLength <= 15000) {
+    if (bookLength >= 700 && bookLength <= 15000 && result > 1) {
         showResult.innerHTML = "Wow, that's a long book! It will take you approximately " + result + ' days to finish this one.'
-    } else {
+    } else if (bookLength < 700 && result > 1) {
         showResult.innerHTML = 'It will take you approximately ' + result + ' days to finish this book.';
+    } else {
+        showResult.innerHTML = "Wow, that's a long book! However, it will take you only " + result + ' day to finish this one.';
     }
 }
 
