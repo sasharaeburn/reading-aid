dfd304a18a87921a4ed55913c7db327d39ccc717
Alex K <contact.alexsophia@gmail.com>
2020-05-22 17:09:20 +0300

    Refactored some code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,17 +22,16 @@ function calculateNeededTime() {
 function getDisplayMessage(result) {
     let bookLength = getBookLength();
 
-    if(result > 1) {
-        if(bookLength >= 700) {
+    if (bookLength >= 700) {
+        if (result > 1) {
             return "Wow, that's a long book! It will take you approximately " + Math.ceil(result) + ' days to finish it.'
         }
-        return 'It will take you approximately ' + Math.ceil(result) + ' days to finish this book.';
+
+        return "Wow, that's a long book! However, it will take you only one day to finish it.";
     }
 
-    if(result === 1) {
-        if(bookLength >= 700) {
-            return "Wow, that's a long book! However, it will take you only one day to finish it.";
-        }
+    if (result > 1) {
+        return 'It will take you approximately ' + Math.ceil(result) + ' days to finish this book.';
     }
 
     return "Seems like you can finish this book in one day.";
