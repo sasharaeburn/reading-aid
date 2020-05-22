ec39d95719a07caaf2824583cd85084e167c3aa1
Alex K <contact.alexsophia@gmail.com>
2020-05-22 16:20:46 +0300

    Added some finishing touches.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let form = document.getElementById('calculate_total');
+let form = document.getElementById('calculate-total');
 form.addEventListener('submit', event => {
     event.preventDefault();
 
@@ -7,22 +7,24 @@ form.addEventListener('submit', event => {
 })
 
 function calculateNeededTime() {
-    const bookLength = parseInt(document.getElementById('book_length').value);
-    const readingAmount = parseInt(document.getElementById('reading_amount').value);
+    const bookLength = parseInt(document.getElementById('book-length').value);
+    const readingAmount = parseInt(document.getElementById('reading-amount').value);
 
     return bookLength / readingAmount;
 }
 
 function displayResult(result) {
     let showResult = document.getElementById('result');
-    let bookLength = parseInt(document.getElementById('book_length').value);
+    let bookLength = parseInt(document.getElementById('book-length').value);
 
     if (bookLength >= 700 && bookLength <= 15000 && result > 1) {
-        showResult.innerHTML = "Wow, that's a long book! It will take you approximately " + result + ' days to finish this one.'
+        showResult.innerHTML = "Wow, that's a long book! It will take you approximately " + result + ' days to finish it.'
     } else if (bookLength < 700 && result > 1) {
         showResult.innerHTML = 'It will take you approximately ' + result + ' days to finish this book.';
+    } else if (!(bookLength < 700 && result === 1)) {
+        showResult.innerHTML = "Wow, that's a long book! However, it will take you only one day to finish it.";
     } else {
-        showResult.innerHTML = "Wow, that's a long book! However, it will take you only " + result + ' day to finish this one.';
+        showResult.innerHTML = "Seems like you can finish this book in one day.";
     }
 }
 
